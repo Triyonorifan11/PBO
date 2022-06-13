49c93772377019d77a954b3c74163411574f598b
Triyono Rifan <triyonorifan01@gmail.com>
2022-06-13 20:59:04 +0700

    update linked list

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <time.h>
 #include <windows.h>
 
-// Nama = Triyono Rifan
-// NPM = 20081010003
+// Nama = 1. Triyono Rifan (20081010003)
+//        2. Alfian Rachmad Dianto ()
 // Kelas = PBO C081
 
 using namespace std;
 
 void mainMenu();
+void dashboard();
+void pelayanan();
+void bookingTempat();
 void pilihMenuPesan();
 void metodeBayarBank();
-
+int nomor_pesanan_note;
+string nama_pemesan_note;
 // simpan data menu
 // class daftar menu
 class Menu
@@ -51,6 +56,9 @@ private:
 
     double harga_pilih = 0;
     double bayar;
+
+public:
+    int nomor_pesanan;
 
 public:
     // nama database txt
@@ -120,8 +128,10 @@ public:
                       << updateNama;
             Db::input << "\t" << updateHarga;
         }
+
         Db::output.close();
         Db::input.close();
+
         if (flag == 0)
         {
             cout << "Data tidak ditemukan!!\n\n"
@@ -131,46 +141,6 @@ public:
         else
         {
             cout << "Data berhasil diupdate !! \n\n"
-                 << endl;
-            remove("Menu_restoran.txt");
-            rename("temp.txt", "Menu_restoran.txt");
-        }
-    }
-
-    void deleteMenu(int indexDelete)
-    {
-        Db::output.open(Db::fileName, ios::in);
-        Db::input.open("temp.txt", ios::app);
-        string hapusNama;
-        double hapusHarga;
-
-        int flag = 0;
-        int index = 1;
-
-        while (!Db::output.eof())
-        {
-            Db::output >> hapusNama;
-            Db::output >> hapusHarga;
-            index++;
-            if (indexDelete != index - 1)
-            {
-                flag = 1;
-                Db::input << "\n"
-                          << hapusNama;
-                Db::input << "\t" << hapusHarga;
-            }
-        }
-        Db::output.close();
-        Db::input.close();
-        if (flag == 0)
-        {
-            cout << "Data tidak ditemukan!!\n\n"
-                 << endl;
-            remove("temp.txt");
-        }
-        else
-        {
-            cout << "Data berhasil dihapus !! \n\n"
                  << endl;
             remove("Menu_restoran.txt");
             rename("temp.txt", "Menu_restoran.txt");
@@ -199,8 +169,50 @@ public:
         }
 
         cout << "\nJumlah Menu: " << index - 1 << endl;
-
         Db::output.close();
+    }
+
+    // delete database menu
+    void deleteMenu(int indexDelete)
+    {
+        Db::output.open(Db::fileName, ios::in);
+        Db::input.open("temp.txt", ios::app);
+
+        string hapusNama;
+        double hapusHarga;
+
+        int util = 0;
+        int index = 1;
+
+        while (!Db::output.eof())
+        {
+            Db::output >> hapusNama;
+            Db::output >> hapusHarga;
+            index++;
+            if (indexDelete != index - 1)
+            {
+                util = 1;
+                Db::input << "\n"
+                          << hapusNama;
+                Db::input << "\t" << hapusHarga;
+            }
+        }
+        Db::output.close();
+        Db::input.close();
+
+        if (util == 0 || indexDelete > index - 1)
+        {
+            cout << "Data tidak ditemukan!!\n\n"
+                 << endl;
+            remove("temp.txt");
+        }
+        else
+        {
+            cout << "Data berhasil dihapus !! \n\n"
+                 << endl;
+            remove("Menu_restoran.txt");
+            rename("temp.txt", "Menu_restoran.txt");
+        }
     }
 
     // untuk pilih menu pesanan dan ambil harga pesanan
@@ -271,6 +283,9 @@ public:
         }
 
         total_bayar = pajak + pesanan - diskon;
+        cout << "\n==============" << endl;
+        cout << "Nomor Pesanan = " << nomor_pesanan_note << endl;
+        cout << "Nama Pemesan = " << nama_pemesan_note << endl;
         cout << "Total pesanan = Rp" << pesanan << endl;
         cout << "Pajak = Rp" << pajak << endl;
         cout << "Diskon = Rp" << diskon << endl;
@@ -328,6 +343,113 @@ public:
     }
 };
 
+// class untuk linked list
+class Node
+{
+public:
+    string nama_pemesan;
+    int nomer_pesanan, nomer_meja;
+    Node *next;
+};
+
+Node *head, *tail, *newNode, *utils, *delete_node, *before;
+
+class SetAntrian : public Node
+{
+public:
+    // set data pertama
+    void set_first_data(string nama_pemesan, int nomer_pesanan, int nomer_meja)
+    {
+        head = new Node();
+        head->nama_pemesan = nama_pemesan;
+        head->nomer_meja = nomer_meja;
+        head->nomer_pesanan = nomer_pesanan;
+        head->next = NULL;
+        tail = head;
+    }
+
+    // jumlahData
+    int count_data()
+    {
+        utils = head;
+        int jumlah = 0;
+        while (utils != NULL)
+        {
+            jumlah++;
+            utils = utils->next;
+        }
+        return jumlah;
+    }
+
+    // tambah data di akhir linked list
+    void add_dataAkhir(string nama_pemesan, int nomer_pesanan, int nomer_meja)
+    {
+        newNode = new Node();
+        newNode->nama_pemesan = nama_pemesan;
+        newNode->nomer_meja = nomer_meja;
+        newNode->nomer_pesanan = nomer_pesanan;
+        newNode->next = NULL;
+        tail->next = newNode;
+        tail = newNode;
+    }
+
+    // edit data tengah sesuai posisi
+    void update_dataPosisi(string nama_pemesan, int nomer_pesanan, int nomer_meja, int posisi)
+    {
+        if (posisi < 1 || posisi > count_data())
+        {
+            cout << "Meja Tidak tersedia" << endl;
+        }
+        // isi /update posisi 1
+        else if (posisi == 1)
+        {
+            head->nama_pemesan = nama_pemesan;
+            head->nomer_pesanan = nomer_pesanan;
+            head->nomer_meja = nomer_meja;
+        }
+        // isi /update posisi terakhir
+        else if (posisi == count_data())
+        {
+            tail->nama_pemesan = nama_pemesan;
+            tail->nomer_pesanan = nomer_pesanan;
+            tail->nomer_meja = nomer_meja;
+        }
+        else
+        {
+            utils = head;
+            int nomor = 1;
+            while (nomor < posisi)
+            {
+                utils = utils->next;
+                nomor++;
+            }
+            utils->nama_pemesan = nama_pemesan;
+            utils->nomer_meja = nomer_meja;
+            utils->nomer_pesanan = nomer_pesanan;
+        }
+    }
+
+    // tampilkan data node
+    void print_data()
+    {
+        utils = head;
+        int i = 1;
+        cout << "No";
+        cout << "  Nama Pemesan";
+        cout << "  Nomer Meja";
+        cout << "  Nomer Pesanan" << endl;
+        while (utils != NULL)
+        {
+            cout << i++;
+            cout << "\t" << utils->nama_pemesan;
+            cout << "\t   Meja " << utils->nomer_meja;
+            cout << "\t  " << utils->nomer_pesanan << endl;
+            utils = utils->next;
+        }
+        cout << "Jumlah data : " << count_data() << endl;
+    }
+};
+
 // 1. fungsi input menu restaurant
 void inputMenu()
 {
@@ -348,31 +470,10 @@ void inputMenu()
     // cin >> tipe;
 
     Menu menu1 = Menu(namaMenu, harga);
+    dataBase_Menu.save(menu1);
     cout << "\n\nData tersimpan !!\n"
          << endl;
     system("pause");
-
-    // switch (tipe)
-    // {
-    // case 1:
-    //     // save data
-    //     dataBase_Menu.save(menu1);
-    //     cout << "\n\nData tersimpan !!\n"
-    //          << endl;
-    //     system("pause");
-    //     break;
-
-    // case 2:
-    //     // dataBase_Minum.save(menu1);
-    //     cout << "\n\nData tersimpan !!\n"
-    //          << endl;
-    //     system("pause");
-    //     break;
-    // default:
-    //     cout << "Salah inputan" << endl;
-    //     cout << "Data tidak Tersimpan" << endl;
-    //     break;
-    // }
 
     mainMenu();
 }
@@ -401,6 +502,7 @@ void pilihMenuPesan()
     Db repo_fee = Db("RepoFee.txt");
 
     int pilihPesanan, porsi, tambah;
+
     system("cls");
     dataBase.showAll();
     cout << "\n\nPilih menu diatas : ";
@@ -443,7 +545,7 @@ void pilihMenuPesan()
     }
 
     system("pause");
-    mainMenu();
+    pelayanan();
 }
 
 // 4. fungsi update data menu
@@ -499,6 +601,71 @@ void metodeBayarBank()
     }
 }
 
+// 7. fungsi Tampil Meja dan daftar antrian
+void showMejaDanAntrian()
+{
+    SetAntrian data_antrian;
+    data_antrian.print_data();
+}
+
+// booking tempat
+void bookingTempat()
+{
+    SetAntrian update_data;
+    string nama_pemesan;
+    int nomer_meja;
+    int nomor_pesanan = rand() % 1000 + 1000;
+
+    showMejaDanAntrian();
+
+    cout << "Booking Meja" << endl;
+    cout << "Nama Pemesan : ";
+    cin >> nama_pemesan;
+    cout << "Nomor pesanan : " << nomor_pesanan << endl;
+    nomor_pesanan_note = nomor_pesanan;
+    nama_pemesan_note = nama_pemesan;
+    cout << "Nomer Meja : ";
+    cin >> nomer_meja;
+    int posisi = nomer_meja;
+
+    update_data.update_dataPosisi(nama_pemesan, nomor_pesanan, nomer_meja, posisi);
+    cout << "Booking Tempat selesai" << endl;
+    system("pause");
+    // pelayanan();
+}
+
+void delete_set_booking()
+{
+    SetAntrian update_data;
+    int nomer_meja;
+    showMejaDanAntrian();
+
+    cout << "Pilih Nomer Meja yang akan dihapus : ";
+    cin >> nomer_meja;
+    int posisi = nomer_meja;
+    update_data.update_dataPosisi("-----", 0, nomer_meja, posisi);
+    cout << "Update Tempat selesai..." << endl;
+    system("pause");
+}
+
+void setDataAntri()
+{
+
+    SetAntrian data_antrian;
+    data_antrian.set_first_data("-----", 0, 1);
+    data_antrian.add_dataAkhir("-----", 0, 2);
+    data_antrian.add_dataAkhir("-----", 0, 3);
+    data_antrian.add_dataAkhir("-----", 0, 4);
+    data_antrian.add_dataAkhir("-----", 0, 5);
+    data_antrian.add_dataAkhir("-----", 0, 6);
+    data_antrian.add_dataAkhir("-----", 0, 7);
+    data_antrian.add_dataAkhir("-----", 0, 8);
+    data_antrian.add_dataAkhir("-----", 0, 9);
+    data_antrian.add_dataAkhir("-----", 0, 10);
+    // data_antrian.print_data();
+    // system("pause");
+}
+
 // tutup Aplikasi
 void close()
 {
@@ -507,10 +674,10 @@ void close()
     cout << "Terima kasih" << endl;
 }
 
-// Main menu
+// Main menu admin
 void mainMenu()
 {
-    ofstream createMakan, createMinum;
+    ofstream createMakan;
     createMakan.open("Menu_restoran.txt", ios::app);
     // createMinum.open("Minum_restoran.txt", ios::app);
     createMakan.close();
@@ -522,10 +689,9 @@ void mainMenu()
     cout << "Pilih Fitur aplikasi" << endl;
     cout << "1. Input Menu" << endl;
     cout << "2. Lihat Menu" << endl;
-    cout << "3. Pesan Menu" << endl;
-    cout << "4. Update Menu" << endl;
-    cout << "5. Hapus Menu" << endl;
-    cout << "6. Tutup Aplikasi" << endl;
+    cout << "3. Update Menu" << endl;
+    cout << "4. Hapus Menu" << endl;
+    cout << "5. Dashboard" << endl;
     cout << "Pilih fitur: ";
     cin >> input;
 
@@ -539,19 +705,15 @@ void mainMenu()
     }
     else if (input == 3)
     {
-        pilihMenuPesan();
+        update();
     }
     else if (input == 4)
     {
-        update();
+        deleteItem();
     }
     else if (input == 5)
     {
-        deleteItem();
-    }
-    else if (input == 6)
-    {
-        close();
+        dashboard();
     }
 
     else
@@ -562,9 +724,105 @@ void mainMenu()
     }
 }
 
-int main(int argc, char const *argv[])
+void pelayanan()
 {
 
-    mainMenu();
+    system("cls");
+    int input;
+    cout << "RESTO PIXEL.ID" << endl;
+    cout << "Pilih Fitur Pelayanan" << endl;
+    cout << "1. Pesan Menu" << endl;
+    cout << "2. Liat Antrian" << endl;
+    cout << "3. Hapus List Antrian" << endl;
+    cout << "4. Dashboard" << endl
+         << endl;
+    cout << "Pilih Pelayanan = ";
+    cin >> input;
+
+    switch (input)
+    {
+    case 1:
+        int pilih;
+        cout << "1. Makan di tempat || 2. Take Away" << endl;
+        cout << "pilih : ";
+        cin >> pilih;
+        if (pilih == 1)
+        {
+            bookingTempat();
+            pilihMenuPesan();
+        }
+        else if (pilih == 2)
+        {
+            string nama_pemesan;
+            int nomor_pesanan = rand() % 1000 + 1000;
+            cout << "Nama Pemesan : ";
+            cin >> nama_pemesan;
+            cout << "Nomor pesanan : " << nomor_pesanan << endl;
+            nomor_pesanan_note = nomor_pesanan;
+            nama_pemesan_note = nama_pemesan;
+            system("pause");
+            pilihMenuPesan();
+        }
+        break;
+
+    case 2:
+        showMejaDanAntrian();
+        system("pause");
+        pelayanan();
+        break;
+    case 3:
+        delete_set_booking();
+        pelayanan();
+        break;
+    case 4:
+        dashboard();
+        break;
+    default:
+        cout
+            << "Menu tidak tersedia"
+            << endl;
+        system("pause");
+        mainMenu();
+    }
+}
+
+void dashboard()
+{
+    system("cls");
+    int input;
+    cout << "RESTO PIXEL.ID" << endl;
+    cout << "====Dashboard====" << endl;
+    cout << "1. Halaman Admin" << endl;
+    cout << "2. Pelayanan" << endl;
+    cout << "3. Close" << endl;
+
+    cout << "Pilih Fitur = ";
+    cin >> input;
+
+    switch (input)
+    {
+    case 1:
+        mainMenu();
+        break;
+    case 2:
+        pelayanan();
+        break;
+    case 3:
+        close();
+        break;
+    default:
+        cout << "Salah input" << endl;
+        system("pause");
+        dashboard();
+        break;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    srand(time(0));
+    setDataAntri();
+    dashboard();
+    // mainMenu();
     return 0;
 }
